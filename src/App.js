635c6bd43261a3a10c08ce1a3239a1b493e358da
marwanseerat <marwanseerat06@gmail.com>
2022-07-29 20:16:51 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import {Routes,BrowserRouter, Route,Link} from "react-router-dom";
-import Footer from './component/Footer';
-import Navbar from './component/Navbar';
-import About from './component/About/About'
-import Contact from './component/Contact/Contact'
-import Courses from './component/Courses/Courses'
-import Home from './component/Home/Home';
-import Post from './Post';
-
-
 
 function App() {
   return (
-    <div>
-<BrowserRouter>
-    <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/About" element={<About />}/>
-        <Route path="/Contact" element={<Contact />}/>
-        <Route path="/Courses" element={<Courses />}/>
-        <Route path="/post" element={<Post />}/>
-      </Routes>
-    </BrowserRouter>
-    <Footer/>
-      
+    <div className="App">
+    group3
     </div>
   );
 }
