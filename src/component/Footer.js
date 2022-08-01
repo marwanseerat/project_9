a9d3318d68aaac67f07a2141bed326b2c946a383
marwanseerat <marwanseerat06@gmail.com>
2022-08-01 11:51:00 +0300

    landing page

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,136 +1,61 @@
 import React from 'react'
+import './footer.css'
 // import emailjs from 'emailjs-com'
 function Footer() {
   return (
     <div>
-        <div
-    className="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn"
-    data-wow-delay="0.1s"
-  >
-    <div className="container py-5">
-      <div className="row g-5">
-        <div className="col-lg-4 col-md-6">
-          <h4 className="text-white mb-3">Quick Link</h4>
-          <a className="btn btn-link" href="">
-            About Us
-          </a>
-          <a className="btn btn-link" href="">
-            Contact Us
-          </a>
-          <a className="btn btn-link" href="">
-            Privacy Policy
-          </a>
-          <a className="btn btn-link" href="">
-            Terms &amp; Condition
-          </a>
-          <a className="btn btn-link" href="">
-            FAQs &amp; Help
-          </a>
-        </div>
-        <div className="col-lg-4 col-md-6">
-          <h4 className="text-white mb-3">Contact</h4>
-          <p className="mb-2">
-            <i className="fa fa-map-marker-alt me-3" />
-            123 Street, New York, USA
-          </p>
-          <p className="mb-2">
-            <i className="fa fa-phone-alt me-3" />
-            +012 345 67890
-          </p>
-          <p className="mb-2">
-            <i className="fa fa-envelope me-3" />
-            info@example.com
-          </p>
-          <div className="d-flex pt-2">
-            <a className="btn btn-outline-light btn-social" href="">
-              <i className="fab fa-twitter" />
-            </a>
-            <a className="btn btn-outline-light btn-social" href="">
-              <i className="fab fa-facebook-f" />
-            </a>
-            <a className="btn btn-outline-light btn-social" href="">
-              <i className="fab fa-youtube" />
-            </a>
-            <a className="btn btn-outline-light btn-social" href="">
-              <i className="fab fa-linkedin-in" />
-            </a>
-          </div>
-        </div>
-        {/* <div className="col-lg-3 col-md-6">
-          <h4 className="text-white mb-3">Gallery</h4>
-          <div className="row g-2 pt-2">
-            <div className="col-4">
-              <img
-                className="img-fluid bg-light p-1"
-                src="img/course-1.jpg"
-                alt=""
-              />
-            </div>
-            <div className="col-4">
-              <img
-                className="img-fluid bg-light p-1"
-                src="img/course-2.jpg"
-                alt=""
-              />
-            </div>
-            <div className="col-4">
-              <img
-                className="img-fluid bg-light p-1"
-                src="img/course-3.jpg"
-                alt=""
-              />
-            </div>
-            <div className="col-4">
-              <img
-                className="img-fluid bg-light p-1"
-                src="img/course-2.jpg"
-                alt=""
-              />
-            </div>
-            <div className="col-4">
-              <img
-                className="img-fluid bg-light p-1"
-                src="img/course-3.jpg"
-                alt=""
-              />
-            </div>
-            <div className="col-4">
-              <img
-                className="img-fluid bg-light p-1"
-                src="img/course-1.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-        </div> */}
-        <div className="col-lg-4 col-md-6">
-          <h4 className="text-white mb-3">Newsletter</h4>
-          <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-          <div className="position-relative mx-auto" style={{ maxWidth: 400 }}>
-            <input
-              className="form-control border-0 w-100 py-3 ps-4 pe-5"
-              type="text"
-              placeholder="Your email"
-            />
-            <button
-              type="button"
-              className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2"
-            >
-              SignUp
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-
+ <footer className="footer-distributed">
+  <div className="footer-left">
+    <h3>
+      Wajih<span>Company</span>
+    </h3>
+    <p className="footer-links">
+      <a href="../Home">Home</a>·<a href="#">Blog</a>·
+      <a href="./About">About</a>.<a href="#">Contact</a>
+    </p>
+    <p className="footer-company-name">FantacyDesign © 2021</p>
   </div>
-  {/* Footer End */}
-  {/* Back to Top */}
-  <a href="#" className="btn btn-lg btn-primary btn-lg-square back-to-top">
-    <i className="bi bi-arrow-up" />
-  </a>
-  {/* JavaScript Libraries */}
-  {/* Template Javascript */}
+  <div className="footer-center">
+    <div>
+      <i className="fa fa-map-marker" />
+      <p>
+        <span>Fifth Area, Aqaba</span> Jordan
+      </p>
+    </div>
+    <div>
+      <i className="fa fa-phone" />
+      <p>+962 771234567</p>
+    </div>
+    <div>
+      <i className="fa fa-envelope" />
+      <p>
+        <a href="mailto:support@company.com">Wajih Companys@gmail.com</a>
+      </p>
+    </div>
+  </div>
+  <div className="footer-right">
+    <p className="footer-company-about">
+      <span>About the company</span>
+      Wajih Company is a interview website for prepration for a technical interview questions
+    </p>
+    <div className="footer-icons">
+      <a href="https://www.facebook.com/interviewbit">
+      <i class="fa-brands fa-facebook-f"></i>
+      </a>
+      <a href="https://twitter.com/interview_bit">
+        <i className="fa fa-twitter" />
+      </a>
+      <a href="https://www.linkedin.com/in/marwa-nseerat-971057227/">
+        <i className="fa fa-linkedin" />
+      </a>
+      <a href="https://github.com/marwanseerat/project_9">
+        <i className="fa fa-github" />
+      </a>
+    </div>
+  </div>
+</footer>
+
+
     </div>
   )
 }
